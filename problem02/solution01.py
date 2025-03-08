08ffa6a73a973252b04601f32ff1389f61699ab8
Nilum Mudaliarachchi <168472757+nilum2002@users.noreply.github.com>
2025-03-09 01:33:36 +0530

    solution 01 change

---
+++ problem02/solution01.py
@@ -0,0 +1,20 @@
+arr = sorted(eval(input()))
+target = int(input())
+n  = []
+for i in range(len(arr)):
+    
+    for j in range(len(arr)):
+        for k in range(len(arr)):
+            for x in range( len(arr)):
+                if arr[i] + arr[j] +arr[k] +arr[x] == target:
+                    sub = []
+                    sub.append(arr[i])
+                    sub.append(arr[j])
+                    sub.append(arr[k])
+                    sub.append(arr[x])
+                    
+                    n.append(sub)
+        
+print(n)
+
+# not the optimal solution - Time complexity is O(N^4) - there are 4 for loops 
